5cb6654bf098f58a06d4e8e1b3c6a4c850e4a255
Liang Zheng <jspop_no1@hotmail.com>
2017-08-20 22:47:58 +0800

    print out some text for branch-msg-append

--- bin/branch-msg-append.js
+++ bin/branch-msg-append.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 
+var sp = require('shell-promise');
+var fs = requrie('fs');
+
+var commitMsgTempFile = process.argv[process.argv.length - 1];
+var commitMsg = fs.readFileSync(commitMsgTempFile, 'utf8');
+
+sp('git branch')
+    .then(function(result) {
+        console.log(result, commitMsg);
+    });
+
 console.log('hello append');
 
 process.argv.forEach(function (val, index, array) {
